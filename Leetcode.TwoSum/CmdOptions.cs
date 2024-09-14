cf4f2ac70e5019da5f8adc9db86fc8cbc5ffe1ac
Oleksii Kolesnyk <oleksii.kolesnyk.w@gmail.com>
2024-09-15 00:04:29 +0300

    Add two sum solution

---
+++ Leetcode.TwoSum/CmdOptions.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Leetcode.TwoSum;
+
+public class CmdOptions
+{
+    [Option('t', "target", Required = true, HelpText = "Sum of two integers from first argument.")]
+    public required int Target { get; set; }
+    
+    [Option('a', "array", Required = true, HelpText = "Array of integers separated by comma.", Separator = ',')]
+    public required IEnumerable<int> Numbers { get; set; }
+
+    [Usage(ApplicationAlias = "twosum")]
+    public static IEnumerable<Example> Examples =>
+        new List<Example>
+        {
+            new("Get two sum", new CmdOptions { Target = 4, Numbers = [1, 2, 3] }),
+        };
+}
--- Leetcode.TwoSum/Program.cs
+++ Leetcode.TwoSum/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// https://leetcode.com/problems/two-sum/
+using CommandLine;
+using Leetcode.TwoSum;
+
+Parser.Default
+    .ParseArguments<CmdOptions>(args)
+    .WithParsed(Run);
+
+static void Run(CmdOptions opts)
+{
+    var twoSum = TwoSum(opts.Numbers.ToArray(), opts.Target);
+    Console.WriteLine("Result: {0}", string.Join(", ", twoSum));
+}
+
+static int[] TwoSum(int[] nums, int target)
+{
+    for (var i = 0; i < nums.Length - 1; i++)
+    {
+        for (var j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+                return [i, j];
+        }
+    }
+
+    return [];
+}
